d68d415d6b6bc9b050c45a78f8420ecb9358bdbe
John Edisson Ortiz <john.ortiz@placetopay.com>
2021-07-19 09:51:22 -0500

    feature: Add a configuration helper

---
+++ src/Helpers/ConfigHelper.php
@@ -0,0 +1,12 @@
+<?php
+namespace Hermes\Helpers;
+
+use Illuminate\Support\Facades\Config;
+
+class ConfigHelper
+{
+    public static function get(string $key)
+    {
+       return Config::get('hermes.' . $key);
+    }
+}
